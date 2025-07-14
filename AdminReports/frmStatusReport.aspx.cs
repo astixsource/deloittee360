6515f3ee96edc8d55c7f4bd249c1967d787d87c4
astixsource <system.backup@astix.in>
2025-07-14 13:05:19 +0530

    Commt for Staging 14Jul

--- AdminReports/frmStatusReport.aspx.cs
+++ AdminReports/frmStatusReport.aspx.cs
@@ -1,44 +1,37 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using System.Web;
-using System.Web.Services;
+using System.Web.Security;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using Newtonsoft.Json;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Net;
 using System.IO;
-using ClosedXML.Excel;
+using System.Threading;
 using System.Drawing;
 
 
-public partial class AdminReports_frmNominationStatusReport : System.Web.UI.Page
+public partial class AdminReports_frmStatusReportNew : System.Web.UI.Page
 {
     int j = 0;
     SqlConnection objCon = default(SqlConnection);
     SqlCommand objCom = default(SqlCommand);
     DataTable dt;
     string strConOLD = System.Configuration.ConfigurationManager.AppSettings["strConn"];
-    // string strCon = HttpContext.Current.Application["DbConnectionString"].ToString();
-    static string strCon = HttpContext.Current.Application["DbConnectionString"].ToString();
+    string strCon = HttpContext.Current.Application["DbConnectionString"].ToString();
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["LoginId"] == null)
-        {
-            Response.Redirect("../Login.aspx");
-            return;
-        }
-
         if (!IsPostBack)
         {
-            hdnLoginId.Value = Session["LoginId"].ToString();
+
             ddlCycle.Items.Insert(0, "Select");
             fnFillCycle();
-            fnFillStatus();
         }
     }
     public void fnFillCycle()
@@ -56,322 +49,71 @@ public partial class AdminReports_frmNominationStatusReport : System.Web.UI.Page
         ddlCycle.DataBind();
     }
 
-    public void fnFillStatus()
+    protected void btnShow_Click(object sender, EventArgs e)
     {
-        SqlConnection con = new SqlConnection(strCon.Split('|')[0]);
-        string com = "spGetstatusListForSurveyCompletion";
-        SqlDataAdapter adpt = new SqlDataAdapter(com, con);
-
-        DataTable dt = new DataTable();
-        adpt.Fill(dt);
-        ddlStatus.DataSource = dt;
-        ddlStatus.DataBind();
-        ddlStatus.DataTextField = "Status";
-        ddlStatus.DataValueField = "StatusID";
-        ddlStatus.DataBind();
+        fnBindGrid();
     }
 
-
-    [System.Web.Services.WebMethod()]
-    public static string GetDetails(string CycleId, string StatusID)
+    public void fnBindGrid()
     {
-        try
-        {
-            SqlConnection Scon = new SqlConnection(strCon.Split('|')[0]);
-            //  SqlConnection Scon = new SqlConnection(ConfigurationManager.AppSettings["strConn"]);
-            SqlCommand Scmd = new SqlCommand();
-            Scmd.Connection = Scon;
-            Scmd.CommandText = "spGetCompletionStatus";
-            Scmd.Parameters.AddWithValue("@CycleId", CycleId);
-            Scmd.Parameters.AddWithValue("@StatusId", StatusID);
-            // Scmd.Parameters.AddWithValue("@ReportDate", ReportDate);
-            Scmd.CommandType = CommandType.StoredProcedure;
-            Scmd.CommandTimeout = 0;
-            SqlDataAdapter Sdap = new SqlDataAdapter(Scmd);
-            DataSet ds = new DataSet();
-            Sdap.Fill(ds);
-            Sdap.Dispose();
-            Scmd.Dispose();
-            Scon.Dispose();
+        objCon = new SqlConnection(strCon.Split('|')[0]);
+        objCom = new SqlCommand("spGetCompletionStatus", objCon);
+        objCom.CommandType = CommandType.StoredProcedure;
+        objCom.Parameters.Add("@CycleID", SqlDbType.Int).Value = ddlCycle.SelectedValue.ToString();
 
-            string[] SkipColumn = new string[7];
-            SkipColumn[0] = "Participant Level";
-            SkipColumn[1] = "ApseNodeId";
-            SkipColumn[2] = "StatusId";
-            SkipColumn[3] = "flgApproved";
-            SkipColumn[4] = "flgFeedbackStarted";
-            SkipColumn[5] = "RspID";
-            SkipColumn[6] = "flgAllowOpenSurvey";
-
-
-            return "0|^|" + dttoHTML(ds.Tables[0], SkipColumn, "Rpt");
-        }
-        catch (Exception ex)
-        {
-            return "1|^|" + ex.Message;
-        }
-    }
-    private static string dttoHTML(DataTable dt, string[] SkipColumn, string lbl)
-    {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sbAlign = new StringBuilder();
-        StringBuilder sbTypeSearch = new StringBuilder();
-
-        sb.Append("<table id='tbl" + lbl + "' class='table table-bordered table-sm tbl-" + lbl + "'>");
-        sb.Append("<thead>");
-        sb.Append("<tr>");
-
-        sb.Append("<th style='min-width: 30px; width: 30px;background: #88bd26;color: #F4F4F4;'>Sr.No</th>");
-        for (int j = 0; j < dt.Columns.Count; j++)
-            if (!SkipColumn.Contains(dt.Columns[j].ColumnName.ToString().Trim()))
-                sb.Append("<th style='background: #88bd26;color: #F4F4F4;'>" + dt.Columns[j].ColumnName.ToString() + "</th>");
-
-        //sb.Append("<th style='display: none;'>Search</th>");
-        sb.Append("<th style='width:12%; background: #88bd26;color: #F4F4F4;'>Action</th>");
-        sb.Append("</tr>");
-        sb.Append("</thead>");
-        sb.Append("<tbody>");
-        for (int i = 0; i < dt.Rows.Count; i++)
-        {
-            sbTypeSearch.Clear();
-
-            sb.Append("<tr>");
-            sb.Append("<td style='text-align: center;'>" + (i + 1).ToString() + "</td>");
-            for (int j = 0; j < dt.Columns.Count; j++)
-                if (!SkipColumn.Contains(dt.Columns[j].ColumnName.ToString().Trim()))
-                {
-                    sbAlign.Clear();
-
-                    sbTypeSearch.Append(dt.Rows[i][j].ToString() + " ");
-
-
-
-                    sb.Append("<td style='text-align: " + sbAlign.ToString() + "; '>" + dt.Rows[i][j].ToString() + "</td>");
-
-
-
-
-                }
-
-            if (dt.Rows[i]["flgAllowOpenSurvey"].ToString() == "1")
-            {
-                sb.Append("<td style='width:12%'><a href = '###' onclick='fnOpenSurvey(\"" + dt.Rows[i]["Participant Name"].ToString().Trim() + "\",\"" + dt.Rows[i]["Rater Name"].ToString().Trim() + "\"," + dt.Rows[i]["RspID"].ToString() + ", this);' style='margin-left: 20px;'>Open Survey</a></td>");
-            }
-            if (dt.Rows[i]["flgAllowOpenSurvey"].ToString() != "1")
-            {
-                sb.Append("<td></td>");
-            }
-
-            //sb.Append("<td iden='Search' style='display: none;'>" + sbTypeSearch.ToString() + "</td>");
-            sb.Append("</tr>");
-        }
-        sb.Append("</tbody>");
-        sb.Append("</table>");
-
-        sbAlign.Clear();
-        sbTypeSearch.Clear();
-        return sb.ToString();
-    }
-
-
-    protected void btnDownload_Click(object sender, EventArgs e)
-    {
-        SqlConnection Scon = new SqlConnection(strCon.Split('|')[0]);
-        //SqlConnection Scon = new SqlConnection(ConfigurationManager.AppSettings["strConn"]);
-        SqlCommand Scmd = new SqlCommand();
-        Scmd.Connection = Scon;
-        Scmd.CommandText = "spGetCompletionStatus";
-        Scmd.Parameters.AddWithValue("@CycleId", ddlCycle.SelectedValue);
-         Scmd.Parameters.AddWithValue("@StatusId", ddlStatus.SelectedValue);
-        Scmd.CommandType = CommandType.StoredProcedure;
-        Scmd.CommandTimeout = 0;
-        SqlDataAdapter Sdap = new SqlDataAdapter(Scmd);
+        objCon.Open();
+        objCom.CommandTimeout = 0;
+        SqlDataAdapter da = new SqlDataAdapter(objCom);
         DataSet ds = new DataSet();
-        Sdap.Fill(ds);
-        Sdap.Dispose();
-        Scmd.Dispose();
-        Scon.Dispose();
+       da.Fill(ds);
+        dt = new DataTable();
+        dt.Clear();
+        da.Fill(dt);
+        grdWSMapping.DataSource = ds;
+        grdWSMapping.DataBind();
 
-        XLWorkbook wb = new XLWorkbook();
-        string[] SkipColumn = new string[7];
-        SkipColumn[0] = "Participant Level";
-        SkipColumn[1] = "ApseNodeId";
-        SkipColumn[2] = "StatusId";
-        SkipColumn[3] = "flgApproved";
-        SkipColumn[4] = "flgFeedbackStarted";
-        SkipColumn[5] = "RspID";
-        SkipColumn[6] = "flgAllowOpenSurvey";
-
-
-        wb = AddWorkSheet(wb, ds.Tables[0], SkipColumn, "Report");
-
-        try
-        {
-            //Export the Excel file.
-            HttpContext.Current.Response.Clear();
-            HttpContext.Current.Response.Buffer = true;
-            HttpContext.Current.Response.Charset = "";
-            HttpContext.Current.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-            HttpContext.Current.Response.AddHeader("content-disposition", "attachment;filename=StatusReport_" + DateTime.Now.ToString("yyyyMMddhhmmss") + ".xlsx");
-            using (MemoryStream MyMemoryStream = new MemoryStream())
-            {
-                wb.SaveAs(MyMemoryStream);
-                MyMemoryStream.WriteTo(HttpContext.Current.Response.OutputStream);
-                HttpContext.Current.Response.Flush();
-                HttpContext.Current.Response.End();
-            }
-        }
-        catch (Exception ex)
-        {
-            //
-        }
     }
-    private static XLWorkbook AddWorkSheet(XLWorkbook wb, DataTable dt, string[] SkipColumn, string Sheetname)
+
+    protected void btnExportReport_Click(object sender, EventArgs e)
     {
-        IXLCell cellStart;
-        IXLCell cellEnd;
-        int k = 0, j = 0;
-        var ws = wb.Worksheets.Add(Sheetname);
-
-        //-----------Header------------------------
-        k++;
-        int FreezeRows = k;
-        cellStart = ws.Cell(k, j + 1);
-        for (int c = 0; c < dt.Columns.Count; c++)
+        Response.Clear();
+        Response.Buffer = true;
+        Response.AddHeader("content-disposition", "attachment;filename=StatusReport_" + DateTime.Now.ToString("dd-MMM-yy") + ".xls");
+        Response.Charset = "";
+        Response.ContentType = "application/vnd.ms-excel";
+        using (StringWriter sw = new StringWriter())
         {
-            if (!SkipColumn.Contains(dt.Columns[c].ColumnName.ToString().Trim().Split('$')[0]))
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+            this.fnBindGrid();
+            grdWSMapping.HeaderRow.BackColor = Color.White;
+            foreach (TableCell cell in grdWSMapping.HeaderRow.Cells)
             {
-                j++;
-                ws.Cell(k, j).Value = dt.Columns[c].ColumnName.ToString().Split('$')[0];
-                ws.Cell(k, j).Style.Alignment.WrapText = true;
-                ws.Cell(k, j).Style.Fill.BackgroundColor = XLColor.FromHtml("#728cd4");
-                ws.Cell(k, j).Style.Font.FontColor = XLColor.FromHtml("#ffffff");
-                ws.Cell(k, j).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                cell.BackColor = grdWSMapping.HeaderStyle.BackColor;
             }
-        }
 
-        //------------Body---------------------------
-        for (int b = 0; b < dt.Rows.Count; b++)
-        {
-            k++; j = 0;
-            for (int c = 0; c < dt.Columns.Count; c++)
+            foreach (GridViewRow row in grdWSMapping.Rows)
             {
-                if (!SkipColumn.Contains(dt.Columns[c].ColumnName.ToString().Trim()))
+                row.BackColor = Color.White;
+                foreach (TableCell cell in row.Cells)
                 {
-                    j++;
-                    ws.Cell(k, j).Style.Alignment.WrapText = true;
-                    ws.Cell(k, j).Value = dt.Rows[b][c].ToString();
-
-                    //if (dt.Columns[c].ColumnName.ToString().Trim().Split('$')[1] == "1")
-                    //    ws.Cell(k, j).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
-                    //else if (dt.Columns[c].ColumnName.ToString().Trim().Split('$')[1] == "2")
-                    //    ws.Cell(k, j).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
-                    //else if (dt.Columns[c].ColumnName.ToString().Trim().Split('$')[1] == "3")
-                    //    ws.Cell(k, j).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+                    cell.CssClass = "textmode";
                 }
             }
+
+            grdWSMapping.RenderControl(hw);
+
+            //style to format numbers to string
+            string style = @"<style> .textmode {border:solid 0.1pt #CCCCCC; } </style>";
+            Response.Write(style);
+            Response.Output.Write(sw.ToString());
+            Response.Flush();
+            Response.End();
         }
-        cellEnd = ws.Cell(k, j);
-
-        ws.Range(cellStart, cellEnd).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
-        ws.Range(cellStart, cellEnd).Style.Alignment.SetVertical(XLAlignmentVerticalValues.Center);
-        ws.Range(cellStart, cellEnd).Style.Border.SetInsideBorder(XLBorderStyleValues.Thin);
-        ws.Range(cellStart, cellEnd).Style.Border.SetOutsideBorder(XLBorderStyleValues.Medium);
-        ws.Range(cellStart, cellEnd).Style.Font.SetFontSize(10);
-        ws.SheetView.FreezeRows(FreezeRows);
-        //ws.Columns().Width = 35;
-
-
-        ws.Row(1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        ws.Columns().AdjustToContents();
-
-        return wb;
     }
 
-
-
-    [System.Web.Services.WebMethod()]
-    public static string fnReOpen_Result(int RspID, int CycleID , int LoginID)
+    public override void VerifyRenderingInServerForm(Control control)
     {
 
-        string jsonData = "";
-        try
-        {
-
-            using (SqlConnection Scon = new SqlConnection(strCon.Split('|')[0]))
-            {
-                using (SqlCommand command = new SqlCommand("spManage_ReOpenSurveyCompletion", Scon))
-                {
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
-                    command.CommandTimeout = 0;
-                    command.Parameters.AddWithValue("@RspId", RspID);
-                    command.Parameters.AddWithValue("@LoginId", LoginID);
-                    Scon.Open();
-                    command.ExecuteNonQuery();
-                    Scon.Close();
-                    jsonData = "1|";
-                }
-
-            }
-        }
-        catch (Exception e)
-        {
-            jsonData = "2|" + e.Message;
-        }
-
-        return jsonData;
-
-    }
-
-    [System.Web.Services.WebMethod()]
-    public static string fnViewNominationedRaterDetail(int ApseNodeId, int CycleID)
-    {
-        string lbl = "Rpt";
-        SqlConnection Scon = new SqlConnection(strCon.Split('|')[0]);
-        //SqlConnection Scon = new SqlConnection(ConfigurationManager.AppSettings["strConn"]);
-        SqlCommand Scmd = new SqlCommand();
-        Scmd.Connection = Scon;
-        Scmd.CommandText = "spGetListOfNominatedRaters";//"[SpVanGetCoveragedetails]";
-        Scmd.Parameters.AddWithValue("@ApseNodeId", ApseNodeId);
-        Scmd.Parameters.AddWithValue("@CycleId", CycleID);
-        Scmd.CommandType = CommandType.StoredProcedure;
-        Scmd.CommandTimeout = 0;
-        SqlDataAdapter Sdap = new SqlDataAdapter(Scmd);
-        DataSet Ds = new DataSet();
-        Sdap.Fill(Ds);
-
-        StringBuilder str = new StringBuilder();
-        if (Ds.Tables[0].Rows.Count > 0)
-        {
-            str.Append("<table id='tbl" + lbl + "' class='table table-bordered table-sm tbl-" + lbl + "'>");
-            // str.Append("<table cellpadding='0' cellspacing='0' style='width:100%; border-bottom:1px solid gray;border-collapse:collapse;' id='tblBasicDetailsInfoDetails' class='table-bordered' ><thead>");
-            str.Append("<thead>");
-            str.Append("<tr>");
-            str.Append("<th style='width:2%; background: #88bd26;color: #F4F4F4;'>Sr.No.</th>");
-            str.Append("<th style='width:7%; background: #88bd26;color: #F4F4F4;'>Rater Name</th>");
-            str.Append("<th style='width:7%; background: #88bd26;color: #F4F4F4;'>Rater Email ID</th>");
-            str.Append("<th style='width:7%; background: #88bd26;color: #F4F4F4;'>Relationship</th>");
-            str.Append("<th style='width:7%; background: #88bd26;color: #F4F4F4;'>Status</th>");
-     
-            str.Append("</tr></thead><tbody>");
-
-            for (int i = 0; i < Ds.Tables[0].Rows.Count; i++)
-            {
-                str.Append("<tr> ");
-                str.Append("<td style='width:2%;text-align:center;'>" + (i + 1) + "</td>");
-
-                str.Append("<td style='width:7%;text-align:left;'>" + Ds.Tables[0].Rows[i]["ApsrName"].ToString() + "</td>");
-                str.Append("<td style='width:7%;text-align:left;'>" + Ds.Tables[0].Rows[i]["Email"].ToString() + "</td>");
-                str.Append("<td style='width:7%;text-align:left;'>" + Ds.Tables[0].Rows[i]["Relationship"].ToString() + "</td>");
-                str.Append("<td style='width:7%;text-align:left;'>" + Ds.Tables[0].Rows[i]["Status"].ToString() + "</td>");
-             
-
-                str.Append("</tr>");
-            }
-            str.Append("</tbody></table>");
-        }
-
-        return str.ToString();
     }
 
 }
